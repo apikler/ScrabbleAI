dd05e0253d366dc136624607a08e8a1f95a38e9d
Andrew Pikler <apikler@gmail.com>
2014-05-20 23:40:26 -0400

    Letters/values on tiles

--- GUI/Tile.pm
+++ GUI/Tile.pm
@@ -7,6 +7,8 @@ use Gtk2 '-init';
 
 use base qw(Gnome2::Canvas::Group);
 
+use Gnome2::Canvas;
+
 use GUI::Utils;
 
 sub new {
@@ -24,8 +26,40 @@ sub new {
 	);
 
 	$self->{tile} = $tile;
+	$self->refresh_text();
 
 	return $self;
+}
+
+sub refresh_text {
+	my ($self) = @_;
+
+	my $letter = uc($self->{tile}->get());
+	my $value = $self->{tile}->get_value();
+
+	if ($self->{letter}) {
+		$self->{letter}->destroy();
+	}
+	if ($self->{value}) {
+		$self->{value}->destroy();
+	}
+
+	$self->{letter} = Gnome2::Canvas::Item->new(
+		$self,
+		'Gnome2::Canvas::Text',
+		text => $letter,
+		family => 'Sans',
+		anchor => 'center',
+		weight => 500,
+	);
+
+	$self->{value} = Gnome2::Canvas::Item->new(
+		$self,
+		'Gnome2::Canvas::Text',
+		text => $value,
+		family => 'Sans',
+		anchor => 'se',
+	);
 }
 
 sub draw {
@@ -35,6 +69,27 @@ sub draw {
 		x1 => 0, y1 => 0,
 		x2 => $side, y2 => $side,
 	);
+
+	# Arbitrary scaling factor for text, based on side length
+	my $scale = $side / 27;
+
+	if ($self->{letter}) {
+		$self->{letter}->set(
+			x => $side/2, y => $side/2,
+			'size-points' => 15*$scale,
+		);
+
+		$self->{letter}->show();
+	}
+
+	if ($self->{value}) {
+		$self->{value}->set(
+			x => $side, y => $side,
+			'size-points' => 6*$scale,
+		);
+
+		$self->{letter}->show();
+	}
 
 	$self->{rect}->show();
 	$self->show();
